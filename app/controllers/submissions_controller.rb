02f67c43b6a56cb714e5489116ba1f16d7c33e61
Matt Yeager <mattyeager@bellsouth.net>
2015-04-06 14:07:00 -0400

    added method showing search results

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -2,6 +2,8 @@ class SubmissionsController < ApplicationController
 require "#{Rails.root}/app/helpers/localeze.rb" 
 
 def localeze
+ 
+
  api_client = LocalezeClient.new 
  returned = api_client.check(@business_to_regester) 
 
@@ -15,12 +17,12 @@ def search_business
 end
 
 def test
+   require "open-uri"
    name = params[:business_name].to_s
    url = URI.escape("http://clientcloud.herokuapp.com/search?key=593264690503&q=#{name}")
    response = open(url).read
-   parsed_business_name = Hash.new
-   parsed_business_name = JSON.parse(response)  
-   raise parsed_business_name.inspect
+   @search_result= JSON.parse(response)
+   render "search_business"
 
 end
 
