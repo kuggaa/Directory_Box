95819b7a4608f9ae00899e3dacc74eca8615b061
Matt Yeager <mattyeager@bellsouth.net>
2015-05-01 14:14:40 -0400

    made root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  
-  get 'search',    to: 'submissions#search_business' 
+  root to: 'submissions#search_business' 
   get 'localeze',  to: 'submissions#test' 
   post 'send',     to: 'submissions#test'
   get 'manage/:id',to: 'submissions#account_manager'
