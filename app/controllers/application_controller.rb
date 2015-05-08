d13af275f2c3049ad16942ca4449a52b71e83f17
Matt Yeager <mattyeager@bellsouth.net>
2015-05-08 11:07:46 -0400

    add direct box to blogTool

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,25 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception 
+  helper_method :templates_index_url, :clients_index_url, :new_template_url, :new_client_url
+
+
+	def templates_index_url
+    	return "#{ENV["BLOGS_API"]}?key=#{session[:key]}"
+	end
+
+	def clients_index_url
+		return "#{ENV["CLIENTS_API"]}?key=#{session[:key]}"
+	end 
+
+	def new_template_url
+    	return "#{ENV["BLOGS_API"]}new_template/?key=#{session[:key]}"
+  	end 
+
+  	def new_client_url
+    	return "#{ENV["CLIENTS_API"]}new?key=#{session[:key]}"
+  	end
+
+
 end
